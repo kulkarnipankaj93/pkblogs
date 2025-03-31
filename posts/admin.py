2a1a34d156c7a4d8f7d0f059b1b982e7e1fe7ed0
kulkarnipankaj93 <kulkarnipankaj93@gmail.com>
2025-03-31 12:06:39 +0530

    Pagination added

--- posts/admin.py
+++ posts/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from .models import Post, Comment
 
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Comment)
+#admin.site.register(Post)
+#admin.site.register(Comment)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ['title', 'slug', 'author', 'status'] # Add functionality to filter on admin page
+    list_display = ['title', 'slug', 'author', 'status'] # display given fields on front page of admin
+    search_fields = ['title', 'body'] # add search bar on admin page to search by 'title' and 'body'
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author', )
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Post
 
 # Create your views here.
@@ -8,6 +9,11 @@ def index(request):
     # posts = Post.objects.filter(status='PUBLISHED')
     posts = Post.published_posts()
     total_posts = Post.total_posts()
+    paginator = Paginator(posts, per_page=2)
+    # get the page number from url, query parameter
+    page_number = request.GET.get('page')
+    # get all posts from that page number
+    posts =paginator.get_page(page_number)
     return render(request, 'posts/index.html', context={'posts': posts, 'total_posts': total_posts})
 
 def post_details(request, post_slug):
